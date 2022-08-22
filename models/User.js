6bcb283180481977add04b70e07a54c27aafc276
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-08-22 16:26:36 -0500

    password hash

--- models/User.js
+++ models/User.js
@@ -1,50 +1,50 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class User extends Model {}
 
-// define table columns and configuration
 User.init(
     {
-        // define an id column
         id: {
-            // use the special Sequelize DataTypes object provide what type of data it is
             type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `not null` option
             allowNull: false,
-            // instruct that this is the primary key
             primaryKey: true,
-            // turn on auto increment
             autoIncrement: true
         },
-        // define a username column
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
-        // define an email column
-        email: 
-        {
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
-            // there cannot be any duplicate email values in this table
             unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating the table data
             validate: {
                 isEmail: true
             }
         },
-        // define a password column
         password: {
             type:DataTypes.STRING,
             allowNull: false,
             validate: {
-                // this means the password must be at least 4 characters long
                 len: [4]
             }
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            //set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
